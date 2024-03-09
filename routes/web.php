2d05d2514211eb05fa258e48a7a9c985d44a78c6
unknown <fauzangustifa@gmail.com>
2024-03-09 23:13:06 +0700

    Update Frontend

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Frontend\UserDashboardController;
 
 
 /*
@@ -20,9 +21,6 @@ Route::get('/', function () {
     return view('frontend.home.home');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -35,4 +33,11 @@ require __DIR__.'/auth.php';
 
 Route::get('admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
 
+Route::get('/dashboard', function () {
+    // return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::group(['middleware' => ['auth', 'verified'], 'prefix' => 'user', 'as' => 'user.' ], function(){
+    Route::get('dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
+});
