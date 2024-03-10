ea7b747540ec14d1301bcb6f778d3d09c6834dc3
unknown <fauzangustifa@gmail.com>
2024-03-10 07:24:22 +0700

    Update Frontend

--- routes/admin.php
+++ routes/admin.php
@@ -3,9 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\ProfileController;
+use App\Http\Controllers\Backend\SliderController;
 
 /*Router Admin*/
 Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
 Route::get('profile', [ProfileController::class, 'index'])->name('profile');
 Route::post('profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
 Route::post('profile/update/password', [ProfileController::class, 'updatePassword'])->name('profile.update.password');
+
+// Route Slider
+Route::resource('slider', SliderController::class);
